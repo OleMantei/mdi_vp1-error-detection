466ef8cf9b4dbfa0c64465925c883a18f71e8908
OleMantei <github@1om.eu>
2023-01-09 15:28:22 +0100

    add outlier and sorting

---
+++ src/data/sortingRule.js
@@ -0,0 +1,13 @@
+export const sortDataItemsByTitle = (dataItems) => {
+  return dataItems.sort((a, b) => a.title.localeCompare(b.title));
+};
+
+export const sortDataItemsById = (dataItems) => {
+  return dataItems.sort((a, b) => a.id.localeCompare(b.id));
+};
+
+export const sortDataItemsByOutlier = (dataItems) => {
+  return dataItems.sort(function (a, b) {
+    return Number(b.outlier) - Number(a.outlier);
+  });
+};
