124b40b7cd21f5c29e0900a25bbd90e6fc12604f
OleMantei <github@1om.eu>
2023-01-23 16:53:36 +0100

    add search functionality

--- src/components/FilterBar.vue
+++ src/components/FilterBar.vue
@@ -10,6 +10,7 @@ import {
   sortDataItemsById,
   sortDataItemsByOutlier,
 } from "../data/sortingRule";
+import { searchDataItems } from "../data/searchService";
 export default {
   name: "FilterBar",
   data: function () {
@@ -22,20 +23,32 @@ export default {
       filteredPercentageDifferenceExpensesActualExpensesPlanned:
         getFilteredPercentageDifferenceExpensesActualExpensesPlanned(),
       selectedFiltering: "Ausreißer",
+      search: "",
+      selectedItems: [],
     };
   },
   computed: {
-    filteredExpensesActualItems: function () {
+    listItems: function () {
+      var searchTerm = this.search;
       var selectedFiltering = this.selectedFiltering;
 
       if (selectedFiltering === "Ausreißer") {
-        return sortDataItemsByOutlier(this.filteredExpensesActual);
+        return searchDataItems(
+          sortDataItemsByOutlier(this.filteredExpensesActual),
+          searchTerm
+        );
       }
       if (selectedFiltering === "ID") {
-        return sortDataItemsById(this.filteredExpensesActual);
+        return searchDataItems(
+          sortDataItemsById(this.filteredExpensesActual),
+          searchTerm
+        );
       }
       if (selectedFiltering === "Titel") {
-        return sortDataItemsByTitle(this.filteredExpensesActual);
+        return searchDataItems(
+          sortDataItemsByTitle(this.filteredExpensesActual),
+          searchTerm
+        );
       }
       return getFilteredExpensesActual();
     },
@@ -47,6 +60,7 @@ export default {
   <div class="filterbar-wrapper bg-blue">
     <div class="pt-4 px-4">
       <v-text-field
+        v-model="search"
         label="Suche"
         hide-details="auto"
         variant="underlined"
@@ -60,43 +74,44 @@ export default {
       ></v-select>
     </div>
     <v-list select-strategy="multiple" bg-color="transparent">
-      <div
-        v-for="(item, index) in filteredExpensesActualItems"
+      <v-list-item
+        v-for="(item, index) in listItems"
         :key="{ index }"
+        :value="item.id"
+        class="px-1"
+        variant="plain"
       >
-        <v-list-item :value="item.id" class="px-1" variant="plain">
-          <template #prepend="{ isActive }">
-            <v-list-item-action start>
-              <v-checkbox-btn :model-value="isActive"></v-checkbox-btn>
-            </v-list-item-action>
+        <template #prepend="{ isActive }">
+          <v-list-item-action start>
+            <v-checkbox-btn :model-value="isActive"></v-checkbox-btn>
+          </v-list-item-action>
+        </template>
+        <v-tooltip
+          :text="`${item.id} ${item.title}`"
+          open-delay="1200"
+          location="bottom"
+        >
+          <template #activator="{ props }">
+            <v-list-item-title v-bind="props"
+              >{{ item.id }} {{ item.title }}</v-list-item-title
+            >
           </template>
-          <v-tooltip
-            :text="`${item.id} ${item.title}`"
-            open-delay="1200"
-            location="bottom"
-          >
+        </v-tooltip>
+        <template #append>
+          <v-tooltip :text="`Ausreißerscore: ${item.outlierScore}%`">
             <template #activator="{ props }">
-              <v-list-item-title v-bind="props"
-                >{{ item.id }} {{ item.title }}</v-list-item-title
+              <v-btn
+                :color="item.outlierScoreColor"
+                icon="mdi-information"
+                variant="text"
+                alt="moin"
+                v-bind="props"
               >
+              </v-btn>
             </template>
           </v-tooltip>
-          <template #append>
-            <v-tooltip :text="`Ausreißerscore: ${item.outlierScore}%`">
-              <template #activator="{ props }">
-                <v-btn
-                  :color="item.outlierScoreColor"
-                  icon="mdi-information"
-                  variant="text"
-                  alt="moin"
-                  v-bind="props"
-                >
-                </v-btn>
-              </template>
-            </v-tooltip>
-          </template>
-        </v-list-item>
-      </div>
+        </template>
+      </v-list-item>
     </v-list>
   </div>
 </template>
